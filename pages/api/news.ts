0a335e75e4523c88500098dc82d865a6caad19d1
jessicasu77 <jessica.su0807@gmail.com>
2022-02-06 15:24:24 -0500

    wrote tests to check that posting to /news with an empty title, content, or no title works.

--- pages/api/news.ts
+++ pages/api/news.ts
@@ -1,26 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next';
 import makeRoute from '../../lib/utils/utils';
-import prisma from '../../prisma/prisma';
-import { NewsWrite, newsWriteSchema } from '../../lib/api/createNewsRequest.dto';
-import { BadRequestError } from '../../lib/utils/errors/badRequestError';
-
-export const getNewsHandler = async (req: NextApiRequest, res: NextApiResponse) => {
-  res.status(200).json({ news: await prisma.news.findMany() });
-};
-
-export const postNewsHandler = async (req: NextApiRequest, res: NextApiResponse) => {
-  const validatedNews = validateCreateNewsRequest(req.body);
-  await prisma.news.create({ data: validatedNews });
-  res.status(201).send('Success!');
-};
+import { getNewsHandler, postNewsHandler } from '../../lib/api/newsLogic';
 
 export default makeRoute().get(getNewsHandler).post(postNewsHandler);
-
-const validateCreateNewsRequest = (maybeNews: unknown) => {
-  try {
-    return newsWriteSchema.parse(maybeNews);
-  } catch (e) {
-    throw new BadRequestError('News must include title + content');
-  }
-};
--- test/api/news.test.ts
+++ test/api/news.test.ts
@@ -1,95 +1,114 @@
-import { getNewsHandler, postNewsHandler} from '../../pages/api/news';
+import { getNewsHandler, postNewsHandler } from '../../lib/api/newsLogic';
 import httpMocks, { Body } from 'node-mocks-http';
 import prisma from '../../prisma/prisma';
-import { News } from '@prisma/client';
-import { NewsWrite } from '../../lib/api/createNewsRequest.dto';
 import { BadRequestError } from '../../lib/utils/errors/badRequestError';
-
-const createNews0:NewsWrite = {
-  title: 'First news',
-  content: ':)'
-};
-
-const createNewsBad:unknown = {
-  title: 'i\'m missing content field!'
-}
-
-const news0:News = {
-  id: 0,
-  title: 'First news',
-  content: ':)',
-  createdAt: new Date('1997-08-17')
-};
-
-const news1 = {
-  id: 1, 
-  title: 'Second news',
-  content: ':))',
-  createdAt: new Date('2001-01-15')
-};
+import {
+  createNews0,
+  createNewsBad,
+  createNewsEmptyContent,
+  createNewsEmptyTitle,
+  createNoTitle,
+  news0,
+  news1,
+} from './examples/newsMockData';
 
 describe('news endpoint', () => {
-
   beforeEach(async () => {
-    await prisma.news.deleteMany({})
-  })
+    await prisma.news.deleteMany({});
+  });
 
   it('should post our first news object', async () => {
     const request = httpMocks.createRequest({
       method: 'POST',
       url: '/api/news',
-      body: createNews0
+      body: createNews0,
     });
-  
+
     const response = httpMocks.createResponse();
-  
+
     await postNewsHandler(request, response);
-  
+
     expect(response.statusCode).toBe(201);
     expect(await prisma.news.findMany({})).toEqual([expect.objectContaining(createNews0)]);
-
   });
 
   it('should fail to post an inadequate news', async () => {
     const request = httpMocks.createRequest({
       method: 'POST',
       url: '/api/news',
-      body: createNewsBad as Body
+      body: createNewsBad as Body,
     });
-  
-    const response = httpMocks.createResponse();
-  
-    expect(postNewsHandler(request, response)).rejects.toThrow(
-      new BadRequestError('News must include title + content'));
 
+    const response = httpMocks.createResponse();
+
+    expect(postNewsHandler(request, response)).rejects.toThrow(
+      new BadRequestError('News must include title + content'),
+    );
+  });
+
+  it('should fail to POST news with an empty title', async () => {
+    const request = httpMocks.createRequest({
+      method: 'POST',
+      url: '/api/news',
+      body: createNewsEmptyTitle as Body,
+    });
+
+    const response = httpMocks.createResponse();
+
+    expect(postNewsHandler(request, response)).rejects.toThrow(
+      new BadRequestError('News must include title + content'),
+    );
+  });
+
+  it('should fail to POST news with an empty content', async () => {
+    const request = httpMocks.createRequest({
+      method: 'POST',
+      url: '/api/news',
+      body: createNewsEmptyContent as Body,
+    });
+
+    const response = httpMocks.createResponse();
+
+    expect(postNewsHandler(request, response)).rejects.toThrow(
+      new BadRequestError('News must include title + content'),
+    );
+  });
+
+  it('should fail to POST news with no title', async () => {
+    const request = httpMocks.createRequest({
+      method: 'POST',
+      url: '/api/news',
+      body: createNoTitle as Body,
+    });
+
+    const response = httpMocks.createResponse();
+
+    expect(postNewsHandler(request, response)).rejects.toThrow(
+      new BadRequestError('News must include title + content'),
+    );
   });
 
   it('should get all news', async () => {
-
-    await prisma.news.createMany({data: [news0, news1]});
+    await prisma.news.createMany({ data: [news0, news1] });
     const request = httpMocks.createRequest({
       method: 'GET',
       url: '/api/news',
     });
-  
+
     const response = httpMocks.createResponse();
-  
+
     await getNewsHandler(request, response);
-  
+
     const data = response._getJSONData(); // short-hand for JSON.parse( response._getData() );
     expect(response.statusCode).toBe(200);
     expect(data.news).toEqual([
-      { ...news0, createdAt: expect.anything() }, { ...news1, createdAt: expect.anything() }
+      { ...news0, createdAt: expect.anything() },
+      { ...news1, createdAt: expect.anything() },
     ]);
   });
 
   it('should be true', async () => {
-    await prisma.news.findMany({})
-    expect(true).toBe(true)
-  })
-}); 
-
-
-
-
-
+    await prisma.news.findMany({});
+    expect(true).toBe(true);
+  });
+});
