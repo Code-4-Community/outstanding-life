b0bad22e7de3c718ddfeef3550b1df47b24fd300
thejamielin <jamie.miki.lin@gmail.com>
2022-02-21 20:22:15 -0500

    documented the news endpoint with swagger

--- pages/api/news.ts
+++ pages/api/news.ts
@@ -2,4 +2,18 @@
 import makeRoute from '../../lib/utils/utils';
 import { getNewsHandler, postNewsHandler } from '../../lib/api/newsLogic';
 
+/**
+ * @swagger
+ * /api/news:
+ *   get:
+ *     description: Returns the news
+ *     responses:
+ *       200:
+ *         description: Successful news retrieval
+ *   post:
+ *     description: Creates valid news 
+ *     responses:
+ *       201:
+ *         description: Successful news creation
+ */
 export default makeRoute().get(getNewsHandler).post(postNewsHandler);
