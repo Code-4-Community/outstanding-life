e96b336990023eece3b43be88374467a038c4b8f
saahilkumar <60633758+saahilkumar@users.noreply.github.com>
2022-02-21 20:48:21 -0500

    still fixing build issues

--- lib/components/navbar/index.tsx
+++ lib/components/navbar/index.tsx
@@ -2,7 +2,7 @@ import NavLink from './NavLink';
 
 const NavBar = (props: any) => (
   <div className="NavBar">
-    {props.navLinks.map((button) => (
+    {props.navLinks.map((button: any) => (
       <NavLink key={button.path} path={button.path} label={button.label} />
     ))}
   </div>
