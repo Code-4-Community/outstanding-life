5ecaa49a04924dfb5c45a2aec073f0467503d487
jessicasu77 <jessica.su0807@gmail.com>
2022-02-27 23:25:32 -0500

    cleaned up migrations into one

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,11 +1,10 @@
 import NextAuth from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
 import CognitoProvider from 'next-auth/providers/cognito';
-import { PrismaAdapter } from "@next-auth/prisma-adapter"
-import prisma from '../../../prisma/prisma'
-// import { PrismaClient } from "@prisma/client"
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import { PrismaClient } from '@prisma/client';
 
-// const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 export default NextAuth({
   adapter: PrismaAdapter(prisma),
@@ -18,17 +17,4 @@ export default NextAuth({
     }),
     // ...add more providers here
   ],
-  events: {
-    createUser: async (message) => {
-      const userId = message.user.id;
-      console.log("USER ID:")
-      console.log(userId);
-      const profile = await prisma.vscProfile.create({
-        data: {
-          userId
-        }
-      })
-      
-    }
-  }
 });
--- prisma/migrations/20220228033309_/migration.sql
+++ prisma/migrations/20220228033309_/migration.sql
@@ -8,6 +8,13 @@
 DROP TABLE "Profile";
 
 -- CreateTable
+CREATE TABLE "VerificationToken" (
+    "identifier" TEXT NOT NULL,
+    "token" TEXT NOT NULL,
+    "expires" TIMESTAMP(3) NOT NULL
+);
+
+-- CreateTable
 CREATE TABLE "Account" (
     "id" TEXT NOT NULL,
     "userId" TEXT NOT NULL,
@@ -40,21 +47,20 @@ CREATE TABLE "Session" (
 -- CreateTable
 CREATE TABLE "User" (
     "id" TEXT NOT NULL,
-    "email" TEXT NOT NULL,
-    "privilegeLevel" "PrivilegeLevel" NOT NULL DEFAULT E'PROFILE',
     "name" TEXT,
+    "email" TEXT,
     "emailVerified" TIMESTAMP(3),
     "image" TEXT,
+    "privilegeLevel" "PrivilegeLevel" NOT NULL DEFAULT E'PROFILE',
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
 
--- CreateTable
-CREATE TABLE "VerificationToken" (
-    "identifier" TEXT NOT NULL,
-    "token" TEXT NOT NULL,
-    "expires" TIMESTAMP(3) NOT NULL
-);
+-- CreateIndex
+CREATE UNIQUE INDEX "VerificationToken_token_key" ON "VerificationToken"("token");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "VerificationToken_identifier_token_key" ON "VerificationToken"("identifier", "token");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Account_provider_providerAccountId_key" ON "Account"("provider", "providerAccountId");
@@ -64,12 +70,6 @@ CREATE UNIQUE INDEX "Session_sessionToken_key" ON "Session"("sessionToken");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
-
--- CreateIndex
-CREATE UNIQUE INDEX "VerificationToken_token_key" ON "VerificationToken"("token");
-
--- CreateIndex
-CREATE UNIQUE INDEX "VerificationToken_identifier_token_key" ON "VerificationToken"("identifier", "token");
 
 -- AddForeignKey
 ALTER TABLE "Account" ADD CONSTRAINT "Account_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
