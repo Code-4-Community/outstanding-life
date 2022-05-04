4000f24a112cb1e6a7290ba352068feb5846f1bc
yanDavid21 <yanDavid21@users.noreply.github.com>
2022-05-04 19:30:07 -0400

    aws client + helper func

--- lib/aws.ts
+++ lib/aws.ts
@@ -1,1 +1,14 @@
 import AWS from 'aws-sdk';
+
+
+const {AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY} = process.env
+const AWS_REGION = 'us-east-2'
+const BUCKET_NAME = 'baseBucket'
+
+AWS.config.update({
+    accessKeyId: AWS_ACCESS_KEY_ID,
+    secretAccessKey: AWS_SECRET_ACCESS_KEY,
+    region: AWS_REGION
+})
+
+export const S3Bucket = new AWS.S3({params: {Bucket: BUCKET_NAME}})
--- lib/utils/utils.ts
+++ lib/utils/utils.ts
@@ -2,6 +2,7 @@ import nc from 'next-connect';
 import { NextApiRequest, NextApiResponse } from 'next';
 import cors from 'cors';
 import { BadRequestError } from './errors/badRequestError';
+import { Buffer } from 'buffer';
 
 export default function makeRoute() {
   return nc<NextApiRequest, NextApiResponse>({
@@ -17,4 +18,9 @@ export default function makeRoute() {
       res.status(404).end('Page is not found');
     },
   }).use(cors());
+}
+
+export function getImageBufferFromBase64(base64string: string) {
+  const cleanedBase64String: string = base64string.replace(/^data:image\/\w+;base64,/, '');
+  return Buffer.from(cleanedBase64String, 'base64');
 }
