147e076f9a452058ecbef908a8dfc724a99ebf3c
saahilkumar <60633758+saahilkumar@users.noreply.github.com>
2022-05-17 15:17:52 -0400

    Removing focus highlight on NavLinks

--- lib/components/navbar/NavLink.tsx
+++ lib/components/navbar/NavLink.tsx
@@ -18,7 +18,10 @@ const NavLink: React.FC<LinkProps> = ({ path, label }) => {
         textUnderlineOffset="3px"
         textDecorationThickness="3px"
         textDecorationColor={'var(--purple)'}
-        tabIndex={0}>
+        tabIndex={0}
+        _focus={{
+          boxShadow: 'none',
+        }}>
         <chakra.span fontWeight="bold" fontSize="24px">
           {label}
         </chakra.span>
