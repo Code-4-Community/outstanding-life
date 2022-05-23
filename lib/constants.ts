983b778bbf41946e9bb90b3a6245f14478f98376
Varun Thakkar <varunthakkar2013@gmail.com>
2022-05-23 19:20:22 -0400

    prettier

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,4 +1,4 @@
 const BASE_URL = process.env.PRODURL ?? 'http://localhost:3000';
-const PAGE_SIZES: string[] = ["base", "sm"]
+const PAGE_SIZES: string[] = ['base', 'sm'];
 
 export { BASE_URL, PAGE_SIZES };
