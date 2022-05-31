1165ee9eee61dfeb224e2e0e800fad5a7d46c33b
Varun Thakkar <varunthakkar2013@gmail.com>
2022-05-31 14:51:21 -0400

    fixes

--- cypress/integration/TC05_partners.spec.js
+++ cypress/integration/TC05_partners.spec.js
@@ -1,11 +1,10 @@
-/// <reference types="cypress" />
 let LOCAL_URL = 'http://localhost:3000/partners';
 
 describe('Partners Flow', () => {
   beforeEach(() => {
     cy.visit(LOCAL_URL);
   });
-  it('partners page is shown correctly', () => {
+  it('should show page content correctly', () => {
     cy.get('[data-cy=partners-header]').should('be.visible');
     cy.get('[data-cy=partners-logos]').should('be.visible');
   });
