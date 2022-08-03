d27a257c97b118af5d5b98318f04135a0e15b021
al-chang <aac6692@gmail.com>
2022-08-03 19:37:10 -0400

    Make improvements to contact us page

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -6,11 +6,12 @@ const ContactUs: React.FC = () => {
   return (
     <>
       <Flex margin="auto" maxWidth="80%" marginTop="50px" direction="column">
-        <Heading>We would love to hear from you and keep you updated with our progress!</Heading>
+        <Heading>We would love to hear from you and keep you updated on our progress!</Heading>
         <Box margin="50px 0">
           <Box margin="25px 0">
             <Text>
-              For questions, press inquiries, or other issues please contact us at{' '}
+              To be added to our mailing and newsletter list, questions, press inquiries, or other
+              issues please contact us at{' '}
               <b>
                 <a href="mailto:info@outstandinglife.org">info@outstandinglife.org</a>
               </b>
