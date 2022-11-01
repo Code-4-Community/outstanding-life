13e7902dd0265ee2311b340eb1a8ab45a8617733
Jennifer Adisoetjahya <ja0911@gmail.com>
2022-11-01 15:00:20 -0400

    remove files out of scope

--- cypress/integration/TC01_about_us.spec.js
+++ cypress/integration/TC01_about_us.spec.js
@@ -1,17 +1,17 @@
 /// <reference types="cypress" />
-let LOCAL_URL = 'http://localhost:3000/about-us';
+let LOCAL_URL = 'http://localhost:3000/about-us'
 
 describe('About Us Flow', () => {
-  beforeEach(() => {
-    cy.visit(LOCAL_URL);
-  });
-  it('learn more section is shown correctly', () => {
-    cy.get('[data-cy=learn-more-section]').should('be.visible');
-  });
-  it('should scroll to display correct content when the learn more button is clicked', () => {
-    cy.get('[data-cy=learn-more-button]').should('be.visible').click();
-    cy.get('[data-cy=mission-statement-section]').should('be.visible');
-    cy.get('[data-cy=need-section]').scrollIntoView().should('be.visible');
-    cy.get('[data-cy=leadership-team-section]').scrollIntoView().should('be.visible');
-  });
-});
+    beforeEach(() => {
+        cy.visit(LOCAL_URL);
+    })
+    it('learn more section is shown correctly', () => {
+        cy.get('[data-cy=learn-more-section]').should('be.visible');
+    })
+    it('should scroll to display correct content when the learn more button is clicked', () => {
+        cy.get('[data-cy=learn-more-button]').should('be.visible').click();
+        cy.get('[data-cy=mission-statement-section]').should('be.visible');
+        cy.get('[data-cy=need-section]').scrollIntoView().should('be.visible');
+        cy.get('[data-cy=leadership-team-section]').scrollIntoView().should('be.visible');
+    })
+})
--- cypress/integration/TC02_navbar.spec.js
+++ cypress/integration/TC02_navbar.spec.js
@@ -1,38 +1,38 @@
 /// <reference types="cypress" />
-let ABOUT_US_URL = 'http://localhost:3000/about-us';
-let DONATE_URL = 'http://localhost:3000/donate';
+let ABOUT_US_URL = 'http://localhost:3000/about-us'
+let DONATE_URL   = 'http://localhost:3000/donate'
 
 describe('NavBar Flow', () => {
-  describe('Standard NavBar', () => {
-    beforeEach(() => {
-      cy.visit(ABOUT_US_URL);
+    describe('Standard NavBar', () => {
+        beforeEach(() => {
+            cy.visit(ABOUT_US_URL);
 
-      // large screen size
-      cy.viewport(1536, 960);
-    });
-    it('navbar is shown correctly', () => {
-      cy.get('[data-cy=standard-navbar]').should('be.visible');
-    });
-    it('should switch to the proper page when a link in the navbar is clicked', () => {
-      cy.get('[data-cy=nav-bar-Donate]').should('be.visible').click();
-      cy.url().should('eq', DONATE_URL);
-    });
-  });
+            // large screen size
+            cy.viewport(1536, 960);
+        })
+        it('navbar is shown correctly', () => {
+            cy.get('[data-cy=standard-navbar]').should('be.visible');
+        })
+        it('should switch to the proper page when a link in the navbar is clicked', () => {
+            cy.get('[data-cy=nav-bar-Donate]').should('be.visible').click();
+            cy.url().should('eq', DONATE_URL);
+        })
+    })
 
-  describe('Hamburger NavBar', () => {
-    beforeEach(() => {
-      cy.visit(ABOUT_US_URL);
+    describe('Hamburger NavBar', () => {
+        beforeEach(() => {
+            cy.visit(ABOUT_US_URL);
 
-      // small screen size
-      cy.viewport(375, 667);
-    });
-    it('hamburger navbar is shown correctly', () => {
-      cy.get('[data-cy=hamburger-navbar-icon]').should('be.visible');
-    });
-    it('should switch to the proper page when a link in the hamburger menu is clicked', () => {
-      cy.get('[data-cy=hamburger-navbar-icon]').should('be.visible').click();
-      cy.get('[data-cy=hamburger-navbar-Donate]').should('be.visible').click();
-      cy.url().should('eq', DONATE_URL);
-    });
-  });
-});
+            // small screen size
+            cy.viewport(375, 667);
+        })
+        it('hamburger navbar is shown correctly', () => {
+            cy.get('[data-cy=hamburger-navbar-icon]').should('be.visible');
+        })
+        it('should switch to the proper page when a link in the hamburger menu is clicked', () => {
+            cy.get('[data-cy=hamburger-navbar-icon]').should('be.visible').click();
+            cy.get('[data-cy=hamburger-navbar-Donate]').should('be.visible').click();
+            cy.url().should('eq', DONATE_URL);
+        })
+    })
+})
