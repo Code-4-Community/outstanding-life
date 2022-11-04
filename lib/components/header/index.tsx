bc727a8ee82a9f25dc9f59eba0a1a97f407a1872
saahilkumar <60633758+saahilkumar@users.noreply.github.com>
2022-11-03 22:51:24 -0400

    Minor tweaks to create/edit buttons

--- lib/components/header/index.tsx
+++ lib/components/header/index.tsx
@@ -1,5 +1,5 @@
-import { Heading, Box, Button, Text, Flex, useBreakpoint } from '@chakra-ui/react';
-import { PAGE_SIZES } from '../../constants';
+import { Heading, Box, Flex } from '@chakra-ui/react';
+import HeaderButton from '../header-button';
 
 interface HeaderProps {
   header: String;
@@ -16,29 +16,11 @@ const Header: React.FC<HeaderProps> = ({
   buttonText,
   onButtonClick,
 }: HeaderProps) => {
-  const breakpoint = useBreakpoint();
-
   return (
     <Box style={style}>
       <Flex flexDir={'row'} justifyContent="space-between" mb={'3px'}>
         <Heading>{header}</Heading>
-        {hasButton && (
-          <Button
-            mt="5px"
-            padding="15px 10px"
-            borderRadius="md"
-            variant="osl"
-            _hover={{ textDecoration: 'none' }}
-            _focus={{ outline: 'none' }}
-            onClick={onButtonClick}>
-            <Text
-              margin="2px"
-              fontSize={breakpoint && PAGE_SIZES.includes(breakpoint) ? 'xs' : 'lg'}
-              fontWeight="bolder">
-              {buttonText}
-            </Text>
-          </Button>
-        )}
+        {hasButton && <HeaderButton buttonText={buttonText} onButtonClick={onButtonClick}/>}
       </Flex>
       <Box h={'4px'} bg={'var(--magenta)'} />
     </Box>
